9adb58fd54d4d8a8783a0f1b133a6d6bf70e5e23
Joker <464616027@qq.com>
2022-05-01 14:13:17 +0800

    init：项目初始化

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.css";
+
+import {DetailPage, HomePage, RegisterPage, SignInPage} from "./pages";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className={styles.App}>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/" component={HomePage}/>
+                    <Route path="/signIn" component={SignInPage}/>
+                    <Route path="/register" component={RegisterPage}/>
+                    <Route path="/detail/:touristRouteId" component={DetailPage}/>
+                    <Route render={() => <h1>404 not found 页面去火星了 ！</h1>}/>
+                </Switch>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
